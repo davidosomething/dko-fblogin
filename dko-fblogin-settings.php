7d13f08c09f2baa7d7ca8ce7bdfe04286f63fd34
David O'Trakoun <dotrakoun@arn.com>
2012-04-12 20:56:12 -0400

    remove redundant ssl check, site_url already does it

--- dko-fblogin-settings.php
+++ dko-fblogin-settings.php
@@ -1,11 +1,11 @@
 <?php
+/**
+ * dko-fblogin-settings.php
+ * make sure WordPress is loaded before requiring this file!
+ */
+
 define('DKOFBLOGIN_SLUG', 'dkofblogin');
-if ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] !== 'off') {
-  define('DKOFBLOGIN_ENDPOINT', str_replace('http://', 'https://', site_url('/register')));
-}
-else {
-  define('DKOFBLOGIN_ENDPOINT', site_url('/register'));
-}
+define('DKOFBLOGIN_ENDPOINT', site_url('/register'));
 
 if (!defined('SERVER_ENVIRONMENT') || SERVER_ENVIRONMENT == 'PROD') {
   $dko_fblogin_http_settings = array(
